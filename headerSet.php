24e5637fac53bba383c82b0972c03dd8febbf2c1
Stanley G <stanleyq75@gmail.com>
2025-05-12 21:46:48 +0700

    modified:   headerSet.php

--- headerSet.php
+++ headerSet.php
@@ -9,3 +9,5 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <!-- Include centralized JS file -->
 <script src="js/script.js"></script>
+
+<script></script>
